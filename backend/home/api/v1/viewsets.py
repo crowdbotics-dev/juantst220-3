6dd80be7d8ad3c6fc13e4ae3eba58d41a8c0fa2c
crowdbotics-dev <engineering@crowdbotics.com>
2024-02-20 14:50:35 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Bank, Bank, Bank
+from .serializers import BankSerializer, BankSerializer, BankSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,12 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+
+class BankViewSet(viewsets.ModelViewSet):
+    serializer_class = BankSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Bank.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Bank(models.Model):
+    "Generated Model"
+    address = models.CharField(
+        max_length=255,
+    )
+    gold = models.BigIntegerField()
